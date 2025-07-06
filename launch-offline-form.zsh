33eced6ec426e6c66b7786afa718d8dfe2ccbaad
Hayden Baldwin <haydenbaldwin9@gmail.com>
2025-07-06 12:34:47 -0500

    Force scale factor to 2.5 for offline form

--- launch-offline-form.zsh
+++ launch-offline-form.zsh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \ --kiosk \ "http://localhost:8971"
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \ --kiosk \ --force-device-scale-factor=2.5 \ "http://localhost:8971"
